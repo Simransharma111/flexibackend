a9b3168c867757c63852f80207efb1006ff10d72
Simran Sharma <ssa13386@gmail.com>
2026-08-10 00:01:12 +0530

    fixed the forgot pass add logs2

--- config/mail.js
+++ config/mail.js
@@ -1,31 +1,16 @@
 import nodemailer from "nodemailer";
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log(
-  "EMAIL_PASSWORD EXISTS:",
-  !!process.env.EMAIL_PASSWORD
-);
-
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
-});
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.error(
-      "EMAIL TRANSPORT ERROR:",
-      error
-    );
-  } else {
-    console.log(
-      "EMAIL SERVER READY:",
-      success
-    );
-  }
+  family: 4,
 });
 
 export default transporter;
