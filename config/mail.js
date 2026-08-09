3107ed3f7158fdbefe4496402b9f587c9ab056a0
Simran Sharma <ssa13386@gmail.com>
2026-08-09 23:32:43 +0530

    fixed the forgot pass

--- config/mail.js
+++ config/mail.js
@@ -1,12 +1,31 @@
 import nodemailer from "nodemailer";
 
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log(
+  "EMAIL_PASSWORD EXISTS:",
+  !!process.env.EMAIL_PASSWORD
+);
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
-
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error(
+      "EMAIL TRANSPORT ERROR:",
+      error
+    );
+  } else {
+    console.log(
+      "EMAIL SERVER READY:",
+      success
+    );
+  }
 });
 
 export default transporter;
